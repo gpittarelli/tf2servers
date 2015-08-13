70f044ba711315ee2da6f9a96d9c5df4aee1726c
George Pittarelli <gjp@cs.umd.edu>
2015-08-12 23:08:17 -0400

    server list styling, with icons

--- src/cljs/tf2servers/core.cljs
+++ src/cljs/tf2servers/core.cljs
@@ -7,6 +7,30 @@
 
 (enable-console-print!)
 
+(defn- make-icon [src desc]
+  (dom/img {:src src
+            :alt desc
+            :title desc
+            :width 12
+            :height 12}))
+
+(def lock-img (make-icon "images/lock.png" "Password protected"))
+(def shield-img (make-icon "images/shield.png" "VAC enabled"))
+(def film-img (make-icon "images/film.png" "Replays enabled"))
+(def bot-img (make-icon "images/bot.png" "Bots on server"))
+(def players-img (make-icon "images/people.png" "Players on server"))
+
+
+(let [tag->icon
+      {"replays" film-img}]
+  (defn maybe-tag->icon
+    "Maps common server tags to an appropriate icon, or just returns
+  the tag if no mapping exists."
+    [tag]
+    (if (contains? tag->icon tag)
+      (tag->icon tag)
+      tag)))
+
 (defn- game-connect [url]
   (set! (.-location js/window) (str "steam://connect/" url)))
 
@@ -17,43 +41,45 @@
   (init-state [_] {:selected-url nil})
   (render-state [_ {:keys [selected-url]}]
     (dom/table
-     (for [server (:servers data)
-           :let [url (str (:ip server) ":" (:port server))
-                 selected (= url selected-url)]]
-       (dom/tr
-        {:data-server url
-         :on-double-click #(game-connect url)
-         :on-click #(om/set-state! owner :selected-url url)
-         :class (when selected "selected")}
-        (dom/td
-         (when (= (get-in server [:info :visibility]) 1)
-           (dom/img {:src "images/lock.png"
-                     :alt "Password protected"
-                     :width 16
-                     :height 16})))
-        (dom/td
-         (when (= (get-in server [:info :vac-enabled?]) 1)
-           (dom/img {:src "images/shield.png"
-                     :alt "VAC enabled"
-                     :width 16
-                     :height 16})))
-        (dom/td
-         (when (get-in server [:info :keywords :replays])
-           (dom/img {:src "images/film.png"
-                     :alt "Replays enabled"
-                     :width 16
-                     :height 16})))
-        (dom/td (get-in server [:info :name]))
-        (dom/td (get-in server [:info :game]))
-        (dom/td (get-in server [:info :bots]))
-        (dom/td (get-in server [:info :players]))
-        (dom/td (get-in server [:info :max-players]))
-        (dom/td (get-in server [:info :map]))
-        (dom/td
-         (->> (get-in server [:info :keywords])
-              (map name)
-              sort
-              (str/join ","))))))))
+     (dom/thead
+      (dom/tr (dom/td {:class "icon-col"} lock-img)
+              (dom/td {:class "icon-col"} shield-img)
+              (dom/td "Server Title")
+              (dom/td "Game")
+              (dom/td {:class "bots"} bot-img)
+              (dom/td {:class "players" :col-span 3} players-img)
+              (dom/td "Map")
+              (dom/td "Tags")))
+     (dom/tbody
+      (for [server (:servers data)
+            :let [url (str (:ip server) ":" (:port server))
+                  selected (= url selected-url)]]
+        (dom/tr
+         {:data-server url
+          :on-double-click #(game-connect url)
+          :on-click #(om/set-state! owner :selected-url url)
+          :class (when selected "selected")}
+         (dom/td (when (= (get-in server [:info :visibility]) 1)
+                   lock-img))
+         (dom/td (when (= (get-in server [:info :vac-enabled?]) 1)
+                   shield-img))
+         (dom/td {:class "name"} (get-in server [:info :name]))
+         (dom/td {:class "game"} (get-in server [:info :game]))
+         (dom/td {:class "bots"}
+                 (let [bot-cnt (get-in server [:info :bots])]
+                   (if (zero? bot-cnt) "-" [bot-cnt bot-img])))
+         (dom/td {:class "player-cnt"} (get-in server [:info :players]))
+         (dom/td {:class "player-sep"} "/")
+         (dom/td {:class "max-players"} (get-in server [:info :max-players]))
+         (dom/td {:class "map"} (get-in server [:info :map]))
+         (dom/td {:class "keys"}
+                 (dom/ul
+                  (->> (get-in server [:info :keywords])
+                       (map name)
+                       sort
+                       (map maybe-tag->icon)
+                       (map dom/li)
+                       (interpose " "))))))))))
 
 (defcomponent app [data owner]
   (init-state [_]
