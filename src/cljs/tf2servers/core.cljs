f9b6fa3498e216294bf8372389848bc1c2c669e8
George Pittarelli <gjp@cs.umd.edu>
2015-08-25 14:18:21 -0400

    continuous scrolling; only display visible rows

--- src/cljs/tf2servers/core.cljs
+++ src/cljs/tf2servers/core.cljs
@@ -116,43 +116,72 @@
                      #(- (.localeCompare (str %1) (str %2))))]
     (om/transact! data :servers #(sort-by key-fn compare-fn %))))
 
-(defn update-col-sizes! [data owner]
-  (let [row (-> (om/get-node owner "table-rows")
+(defn- maybe-set-state [owner key data]
+  (let [old (om/get-state owner key)]
+    (when (not= old data)
+      (om/set-state! owner key data))))
+
+(defn update-measurements! [data owner]
+  (let [table (om/get-node owner "server-table")
+        row (-> (om/get-node owner "table-rows")
                 .-children
-                (aget 0)
+                (aget 1)
                 .-children
                 array-seq)
-        widths (doall (for [td row] (.-clientWidth td)))]
-    (om/set-state! owner :col-widths widths)))
+        scroller (.-parentNode table)
+
+        col-widths (doall (for [td row] (.-clientWidth td)))
+        row-height (.-clientHeight (first row))
+        view-height (.-clientHeight scroller)
+        scroll-top (.-scrollTop scroller)]
+    (doseq [[k d] [[:col-widths col-widths]
+                   [:row-height row-height]
+                   [:view-height view-height]
+                   [:scroll-top scroll-top]]]
+      (maybe-set-state owner k d))))
 
 (defcomponent server-table-row [data owner]
+  (display-name [_] "server-table-row")
   (render [_]
     (let [{data :data
            {:keys [url server]} :server} data]
       (dom/tr
-       {:data-server url
-        :on-double-click #(game-connect url)
-        :on-click #(om/update! data :selected-url url)
-        :class (when (= url (:selected-url data)) "selected")}
-       (map (fn [{:keys [class content attrs]}]
-              (dom/td
-               (merge {:class class
-                       :title (when (string? content) content)}
-                      attrs)
-               content))
-            server)))))
+        {:data-server url
+         :on-double-click #(game-connect url)
+         :on-click #(om/update! data :selected-url url)
+         :class (when (= url (:selected-url data)) "selected")}
+        (map (fn [{:keys [class content attrs]}]
+               (dom/td
+                 (merge {:class class
+                         :title (when (string? content) content)}
+                        attrs)
+                 content))
+             server)))))
+
+(defn pad
+  "Ensure a given seq is always at least n elements long, adding as
+  many pad elements to the end as necessary."
+  [n pad coll]
+  (take n (concat coll (repeat pad))))
 
 (defcomponent server-table [data owner]
+  (display-name [_] "server-table")
   (init-state [_]
     {:selected-col nil
      :sort-order :up
-     :col-widths (repeat 10 nil)})
+     :col-widths (repeat 10 nil)
+     :view-height 1
+     :row-height 1
+     :scroll-top 0})
   (render-state [_ {:keys [selected-url selected-col
-                           sort-order col-widths]}]
+                           sort-order col-widths
+                           view-height row-height scroll-top]}]
     (dom/div {:class "server-table-container"}
       (dom/ul {:class "table-header" :ref "table-header"}
+
         (map
          (fn [idx {:keys [class header attrs]} width]
+
            (let [attrs (merge
                         attrs
                         {:class (str class
@@ -169,30 +198,52 @@
          server-table-columns
          col-widths))
       (dom/div {:class "server-table-scroller"}
-        (dom/table {:class "server-table"}
+        (dom/table {:class "server-table" :ref "server-table"}
           (dom/colgroup
             (map #(dom/col {:class %1})
                  ["icon-col" "icon-col" "title" "game" "icon-col bot-col"
                   "players" "map" "tags"]))
           (dom/tbody {:ref "table-rows"}
-            (map (fn [server]
-                   (om/build server-table-row
-                             {:data data
-                              :server server}
-                             {:key (:url server)}))
-                 (:servers data)))))))
+            (let [->row #(om/build server-table-row
+                                   {:data data :server %}
+                                   {:key (:url %)})
+                  start-row (int (/ scroll-top row-height))
+                  vis-row-cnt (+ 2 (int (/ view-height row-height)))
+                  loading-row (dom/tr {:class "loading"}
+                                (dom/td {:col-span 8}
+                                  "Loading..."))]
+              (println scroll-top start-row
+                       vis-row-cnt view-height row-height)
+
+              [(dom/tr {:class "top-spacer"
+                        :style {:height (* start-row
+                                           row-height)}})
+               (->> (:servers data)
+                    (drop start-row)
+                    (take vis-row-cnt)
+                    (map ->row)
+                    (pad vis-row-cnt loading-row))
+               (dom/tr {:class "bottom-spacer"
+                        :style {:top (* row-height 100)}}
+                 (dom/td "asdfasdf"))]))))))
   (did-mount [_]
-    (let [update-sizes! #(update-col-sizes! data owner)]
+    (let [update-sizes! #(update-measurements! data owner)]
       (.addEventListener js/window "resize" update-sizes!)
+      (.addEventListener (.-parentNode (om/get-node owner "server-table"))
+                         "scroll"
+                         update-sizes!)
       (update-sizes!)))
   (did-update [_ _ _]
-    (update-col-sizes! data owner)))
+    (update-measurements! data owner)))
 
 (defcomponent filter-controls [data owner]
+  (display-name [_] "filter-controls")
   (render [_]
-    (dom/header)))
+    (dom/header
+      (dom/input {:type "number" :min 1 :max 32 :value 24 :step 1}))))
 
 (defcomponent app [data owner]
+  (display-name [_] "app")
   (init-state [_]
     (GET "/api/servers"
         {:handler
